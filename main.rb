8551fadaaa79ed86298d13bd7c5d3bae3cb0e716
Abby Howell <abby.g.howell@gmail.com>
2013-10-02 17:10:10 -0400

    hex colors working

--- main.rb
+++ main.rb
@@ -32,11 +32,13 @@ def zip_codes(string)
 	string.scan(/\b\d{5}\b/)
 end
 
-def hex_colors 
+def hex_colors(string)
+	string.scan(/#([0-9a-fA-F]{6}\b)/).flatten
 end
 
 p email_addresses(@source_text)
 p valid_phone_numbers(@source_text)
 p zip_codes(@source_text)
+p hex_colors(@source_text)
 
 
