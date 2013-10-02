7eec93e3fb023938044ffe09f2042f99f8a997e5
Abby Howell <abby.g.howell@gmail.com>
2013-10-02 16:30:39 -0400

    emails working, phone numbers in progress

--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
 require_relative 'source'
 require 'pry'
 
+# puts @source_text
+
+
+
+# string.scan(pattern)
+
 def valid_ip_addresses 
 end 
 
@@ -10,13 +16,15 @@ end
 def non_mit_ip_addresses 
 end 
 
-def valid_phone_numbers 
+def valid_phone_numbers(string)
+	string.scan(/(1[-\/ ])?\(?([2-9][0-9][0-9])?\)?[-\/ ]?([2-9][1-9][1-9])([-\/ ])(\d{4})/)
 end
 
 def area_codes 
 end
 
-def email_addresses 
+def email_addresses(string)
+	string.scan(/[a-z0-9A-Z_-]+@[a-z0-9A-Z_-]+.[a-z]{3}/)
 end
 
 def zip_codes 
@@ -24,3 +32,7 @@ end
 
 def hex_colors 
 end
+
+email_addresses = email_addresses(@source_text)
+valid_phone_numbers = valid_phone_numbers(@source_text)
+puts valid_phone_numbers.join(" ")
